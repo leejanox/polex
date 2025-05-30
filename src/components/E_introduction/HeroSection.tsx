538d5b3b7f4ed0ede59303973b44c3c4da6c50fe
leejanox <rhdms6748@gmail.com>
2025-05-30 11:44:16 +0900

    .

--- src/components/E_introduction/HeroSection.tsx
+++ src/components/E_introduction/HeroSection.tsx
@@ -1,6 +1,5 @@
-import { U_Button } from '@components/commons/Buttons';
 import { EDdescriptionKorean, EDParticipationList, EDInfo } from './text';
-import styles from '@styles/E_introduction.module.scss';
+import styles from '@styles/E_intro.module.scss';
 import ReactPlayer from 'react-player';
 import clsx from 'clsx';
 import { Link } from 'react-router-dom';
@@ -31,72 +30,72 @@ export const HeroSection = () => {
                     loop={true} muted={true} controls={false} playing={true}
                 />
             </div>
-            <div className={clsx(styles.buttonGroup)}>
-                    <Link to={'/학과소개'}>
-                        <U_Button>
-                            <p>{`학과 소개 \u27AF`}</p>
-                        </U_Button>
-                    </Link>
-                    <a href={'#section2'}>
-                        <U_Button>
-                            <p>{`오시는길 \u27AF`}</p>
-                        </U_Button>
-                    </a>
-                    <a href={'#section3'}>
-                        <U_Button>
-                            <p>{`방명록 남기기 \u27AF`}</p>
-                        </U_Button>
-                    </a>
+            <div className={clsx(styles.buttonGroup1)}>
+                <Link to={'/학과소개'}>
+                    <button>
+                        <p>{`학과 소개 \u27AF`}</p>
+                    </button>
+                </Link>
+                <a href={'#section2'}>
+                    <button>
+                        <p>{`오시는길 \u27AF`}</p>
+                    </button>
+                </a>
+                <a href={'#section3'}>
+                    <button>
+                        <p>{`방명록 남기기 \u27AF`}</p>
+                    </button>
+                </a>
+        </div>
+        <div className={clsx(styles.descriptionContainer)}>
+            <div className={clsx(styles.introduction,styles.left)}>
+                <h1>전시 소개</h1>
+                <div className={clsx(styles.description)}>
+                    <div className={clsx(styles.description,styles.korean)}>
+                        {EDdescriptionKorean.split('\n').map((line, index) => (
+                            <p key={index}>{line}</p>
+                        ))}
+                    </div>
+                </div>
             </div>
-            <div className={clsx(styles.descriptionContainer)}>
-                <div className={clsx(styles.introduction,styles.left)}>
-                    <h1>전시 소개</h1>
-                    <div className={clsx(styles.description)}>
-                        <div className={clsx(styles.description,styles.korean)}>
-                            {EDdescriptionKorean.split('\n').map((line, index) => (
-                                <p key={index}>{line}</p>
-                            ))}
+            <div className={clsx(styles.participation,styles.right)}>
+                <div className={clsx(styles.people)}>
+                    <h1>참여자</h1>
+                    {Object.keys(EDParticipationList).map((key)=>(
+                        <div key={key} className={clsx(styles.item)}>
+                            <h2>{key}</h2>
+                            <p>{EDParticipationList[key as keyof typeof EDParticipationList].value}</p>
                         </div>
-                    </div>
+                    ))}
                 </div>
-                <div className={clsx(styles.participation,styles.right)}>
-                    <div className={clsx(styles.people)}>
-                        <h1>참여자</h1>
-                        {Object.keys(EDParticipationList).map((key)=>(
-                            <div key={key} className={clsx(styles.item)}>
-                                <h2>{key}</h2>
-                                <p>{EDParticipationList[key as keyof typeof EDParticipationList].value}</p>
-                            </div>
-                        ))}
-                    </div>
-                    <div className={clsx(styles.info)}>
-                        <h1>전시 정보</h1>
-                        {Object.keys(EDInfo).map((key)=>(
-                            <div key={key} className={clsx(styles.item)}>
-                                <h2>{key}</h2>
-                                <p>{EDInfo[key as keyof typeof EDInfo].value}</p>
-                            </div>
-                        ))}
-                    </div>
+                <div className={clsx(styles.info)}>
+                    <h1>전시 정보</h1>
+                    {Object.keys(EDInfo).map((key)=>(
+                        <div key={key} className={clsx(styles.item)}>
+                            <h2>{key}</h2>
+                            <p>{EDInfo[key as keyof typeof EDInfo].value}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
-            <div className={clsx(styles.buttonGroup)}>
-                <h1>작품 보러가기</h1>
-                <ArrowRight/>
-                <Link to={'/junior'}>
-                    <U_Button>
-                        <p>1학년 작품</p>
-                    </U_Button>
-                </Link>
-                <Link to={'/senior'}>
-                    <U_Button>
-                        <p>2학년 작품</p>
-                    </U_Button>
-                </Link>
-                <Link to={'/highTech'}>
-                    <U_Button>
-                        <p>하이테크 작품</p>
-                    </U_Button>
+        </div>
+        <div className={clsx(styles.buttonGroup2)}>
+            <h1>작품 보러가기</h1>
+            <ArrowRight/>
+            <Link to={'/junior'}>
+                <button>
+                    <p>1학년</p>
+                </button>
+            </Link>
+            <Link to={'/senior'}>
+                <button>
+                    <p>2학년</p>
+                </button>
+            </Link>
+            <Link to={'/highTech'}>
+                <button>
+                    <p>하이테크</p>
+                </button>
                 </Link>
             </div>
         </div>
